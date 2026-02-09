477ccf0270ecfa4d1346a3a33cf0dfa2c6341a45
Shaamam <shaama0704@gmail.com>
2026-02-09 22:37:21 +0530

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/test/java/io/shaama/rulesengine/RulesengineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shaama.rulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
